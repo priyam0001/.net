eb99dc0ffdd8c0c383cc58ed52d31e9da7274b3f
priyam0001 <50718425+priyam0001@users.noreply.github.com>
2020-01-20 14:54:16 -0500

    first commit

---
+++ lab 2/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+
+            string[] products = {"laptop", "tablet" , "desktop" };
+          
+            ViewData["product"] = products;
+            return View();
+        }
+        public String Details(String[] product) {
+
+
+            String Message = "you have selected " + product;
+            return Message ;
+        }
+    }
+}
